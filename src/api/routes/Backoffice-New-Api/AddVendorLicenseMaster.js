2963c0bc175a9652837e270fde4c232d8e9e239f
Tapan Vyas <tapan-globaltrendz@Tapans-MacBook-Air.local>
2020-05-14 12:08:46 +0530

    new

--- src/api/routes/Backoffice-New-Api/AddVendorLicenseMaster.js
+++ src/api/routes/Backoffice-New-Api/AddVendorLicenseMaster.js
@@ -5,12 +5,24 @@ const dbConnection = require("../../../utilities/db1");
 
 
 
-router.get("/", function(request, response){
+router.post("/", function(request, response){
    
+ 
+    var type = request.body.type;
+    var status = request.body.status;
+    var updatedby = request.body.updatedby;
+    var updatedon = request.body.updatedon;
 
     try{
         const req = new sql.Request(dbConnection);
-        req.execute("dbo.Get_SocksColorList", function(err, data){
+
+
+        req.input('type',sql.NVarChar(100), type);
+        req.input('status',sql.NVarChar(10), status);
+        req.input('updatedon',sql.NVarChar(100), updatedon);
+        req.input('updatedby',sql.Int, updatedby);
+
+        req.execute("dbo.Add_VendorLicenseMaster", function(err, data){
             if(err){
                 console.log("Error while executing the SP - [error] " + err);
                 response.status(404).json({
