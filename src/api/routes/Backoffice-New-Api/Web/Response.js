5d16de10d37dd7cf0a4653ecd15db82d641924f5
Nidhi <tncombo2210@gmail.com>
2020-07-17 16:34:23 +0530

    new

--- src/api/routes/Backoffice-New-Api/Web/Response.js
+++ src/api/routes/Backoffice-New-Api/Web/Response.js
@@ -50,70 +50,74 @@ router.post("/", function(req, res){
 	
 	var msg = 'Payment failed for Hash not verified...<br />Check Console Log for full response...';
 	//Comapre status and hash. Hash verification is mandatory.
+	// if(calchash == resphash)
+	// {
+	// 	msg = 'Transaction Successful and Hash Verified...<br />Check Console Log for full response...';
+	// 	console.log('success')
+	// 	res.render(__dirname+"/Resp.html", {txnid: txnid,amount: amount, productinfo: productinfo, 
+	// 		additionalcharges:additionalcharges,firstname: firstname, email: email, mihpayid : mihpayid, status: 'success',resphash: resphash,msg:msg,verified:'Yes'});
+			
+	// }else
+	// {
+	// 	console.log('failed')
+	// 	res.render(__dirname+"/Resp.html", {txnid: txnid,amount: amount, productinfo: productinfo, 
+	// 		additionalcharges:additionalcharges,firstname: firstname, email: email, mihpayid : mihpayid, status: 'fail',resphash: resphash,msg:msg,verified:'No'});
+			
+	// }
+
+
 	if(calchash == resphash)
-	{
-		msg = 'Transaction Successful and Hash Verified...<br />Check Console Log for full response...';
-		console.log('success')
-		res.render(__dirname+"/Resp.html", {txnid: txnid,amount: amount, productinfo: productinfo, 
-			additionalcharges:additionalcharges,firstname: firstname, email: email, mihpayid : mihpayid, status: 'success',resphash: resphash,msg:msg,verified:'Yes'});
-			
-	}else
-	{
-		console.log('failed')
-		res.render(__dirname+"/Resp.html", {txnid: txnid,amount: amount, productinfo: productinfo, 
-			additionalcharges:additionalcharges,firstname: firstname, email: email, mihpayid : mihpayid, status: 'fail',resphash: resphash,msg:msg,verified:'No'});
-			
-	}
+	msg = 'Transaction Successful and Hash Verified...<br />Check Console Log for full response...';
 		
 	
 	// console.log(req.body);
 	
 	//Verify Payment routine to double check payment
-	// var command = "verify_payment";
+	var command = "verify_payment";
 	
-	// var hash_str = key  + '|' + command + '|' + txnid + '|' + salt ;
-	// var vcryp = crypto.createHash('sha512');	
-	// vcryp.update(hash_str);
-	// var vhash = vcryp.digest('hex');
+	var hash_str = key  + '|' + command + '|' + txnid + '|' + salt ;
+	var vcryp = crypto.createHash('sha512');	
+	vcryp.update(hash_str);
+	var vhash = vcryp.digest('hex');
 	
-	// var vdata='';
-	// var details='';
+	var vdata='';
+	var details='';
 	
-	// var options = {
-	// 	method: 'POST',
-	// 	uri: 'https://info.payu.in/merchant/postservice.php?form=2',
-	// 	form: {
-	// 		key: key,
-	// 		hash: vhash,
-	// 		var1: txnid,
-	// 		command: command
-	// 	},
-	// 	headers: {
-	// 		//  'content-type': 'application/x-www-form-urlencoded' 
-	// 	}
-	// };
+	var options = {
+		method: 'POST',
+		uri: 'https://info.payu.in/merchant/postservice.php?form=2',
+		form: {
+			key: key,
+			hash: vhash,
+			var1: txnid,
+			command: command
+		},
+		headers: {
+			//  'content-type': 'application/x-www-form-urlencoded' 
+		}
+	};
 	
 
-	// function callback(error, response, body) {
-	// 	if (!error && response.statusCode == 200) {
-	// 			vdata = JSON.parse(body);	
-	// 			if(vdata.status == '1')
-	// 			{
-	// 				details = vdata.transaction_details[txnid];
-	// 				console.log(details['status'] + '   ' + details['mihpayid']);
-	// 				if(details['status'] == 'success' && details['mihpayid'] == mihpayid)
-	// 					verified ="Yes";
-	// 				else
-	// 					verified = "No";
-	// 					// res.sendFile(__dirname+"/Resp.html");
-	// 				res.render(__dirname+"/Resp.html", {txnid: txnid,amount: amount, productinfo: productinfo, 
-	// additionalcharges:additionalcharges,firstname: firstname, email: email, mihpayid : mihpayid, status: status,resphash: resphash,msg:msg,verified:verified});
-	// 			}
+	function callback(error, response, body) {
+		if (!error && response.statusCode == 200) {
+				vdata = JSON.parse(body);	
+				if(vdata.status == '1')
+				{
+					details = vdata.transaction_details[txnid];
+					console.log(details['status'] + '   ' + details['mihpayid']);
+					if(details['status'] == 'success' && details['mihpayid'] == mihpayid)
+						verified ="Yes";
+					else
+						verified = "No";
+						// res.sendFile(__dirname+"/Resp.html");
+					res.render(__dirname+"/Resp.html", {txnid: txnid,amount: amount, productinfo: productinfo, 
+	additionalcharges:additionalcharges,firstname: firstname, email: email, mihpayid : mihpayid, status: status,resphash: resphash,msg:msg,verified:verified});
+				}
 		
-	// 	}
-	// }
+		}
+	}
 	
-	// request(options, callback);
+	request(options, callback);
 
 	
 
