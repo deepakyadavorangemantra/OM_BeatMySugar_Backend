d79300d77e787f227b2d2f097832d89da8725d97
Nidhi <tncombo2210@gmail.com>
2020-10-29 18:53:50 +0530

    new

--- src/server.js
+++ src/server.js
@@ -12,27 +12,3 @@ console.log("listening at port ", port);
 
 
 
-
-
-// {
-//     "server" : "192.227.85.199",
-//     "user" : "bmsdev",
-//     "password" : "ei10e9E%",
-//     "database" : "bmsdev",
-//     "port" : 1433,
-//     "options" : {
-//         "encrypt" : false
-//     }
-// }
-
-
-// {
-//     "server" : "192.227.85.199",
-//     "user" : "bmsdev2_db",
-//     "password" : "dc*5fR86",
-//     "database" : "bmsdev2",
-//     "port" : 1433,
-//     "options" : {
-//         "encrypt" : false
-//     }
-// }
