18dc9c3504b0195714d4655acc21e40337841c03
deepak <93deep93@gmail.com>
2020-11-06 17:34:44 +0530

    correction in add and update api.

--- src/api/routes/Backoffice-New-Api/EducationModule/AddTopic.js
+++ src/api/routes/Backoffice-New-Api/EducationModule/AddTopic.js
@@ -8,29 +8,27 @@ const dbConnection = require("../../../../utilities/db1");
 router.post("/", function(request, response){
    
     var chapterid = request.body.chapterid;
-    var title = request.body.orderdetailid;
+    var title = request.body.title;
     var content = request.body.content;
     var orderno = request.body.orderno;
     var img_url = request.body.img_url;
     var type = request.body.type;
-    var createdat = request.body.createdat;
-    var updatedat = request.body.updatedat;
+    var createdon = request.body.createdon;
+    var createdby = request.body.createdby;
     var status = request.body.status;
     
 
     try{
         const req = new sql.Request(dbConnection);
-
-           
         req.input('chapterid',sql.Int, chapterid);
         req.input('title',sql.NVarChar(200), title);
-        req.input('content',sql.NVarChar(200), content);
+        req.input('content',sql.Text, content);
         req.input('orderno',sql.Int, orderno);
         req.input('type',sql.NVarChar, type);
         req.input('img_url',sql.NVarChar, img_url);
-        req.input('createdat',sql.NVarChar(100), createdat);
-        req.input('updatedat',sql.Int, updatedat);
-        req.input('status',sql.Int, status);
+        req.input('createdby',sql.Int, createdby);
+        req.input('createdon',sql.NVarChar(100), createdon);
+        req.input('status',sql.NVarChar(100), status);
 
         req.execute("dbo.Add_Topic", function(err, data){
             if(err){
--- src/api/routes/Backoffice-New-Api/EducationModule/UpdateTopic.js
+++ src/api/routes/Backoffice-New-Api/EducationModule/UpdateTopic.js
@@ -8,13 +8,12 @@ const dbConnection = require("../../../../utilities/db1");
 router.post("/", function(request, response){
    
     
-    var title = request.body.orderdetailid;
+    var title = request.body.title;
     var content = request.body.content;
     var orderno = request.body.orderno;
     var img_url = request.body.img_url;
     var type = request.body.type;
-    var createdat = request.body.createdat;
-    var updatedat = request.body.updatedat;
+    var updatedon = request.body.updatedon;
     var status = request.body.status;
     var topicid = request.body.topicid;
 
@@ -23,13 +22,12 @@ router.post("/", function(request, response){
          
         req.input('topicid',sql.Int, topicid);
         req.input('title',sql.NVarChar(200), title);
-        req.input('content',sql.NVarChar(200), content);
+        req.input('content',sql.Text(200), content);
         req.input('orderno',sql.Int, orderno);
-        req.input('type',sql.NVarChar, type);
+        req.input('type',sql.NVarChar(100), type);
         req.input('img_url',sql.NVarChar, img_url);
-        req.input('createdat',sql.NVarChar(100), createdat);
-        req.input('updatedat',sql.Int, updatedat);
-        req.input('status',sql.Int, status);
+        req.input('updatedon',sql.NVarChar(100), updatedon);
+        req.input('status',sql.NVarChar(10), status);
 
 
         req.execute("dbo.Update_TopicMaster", function(err, data){
