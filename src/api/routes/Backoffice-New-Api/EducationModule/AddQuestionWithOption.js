2f26bc2281eb9a3a1e615f3551f892ea995f9e89
Your Name <you@example.com>
2020-11-10 15:53:29 +0530

    modified api

--- src/api/routes/Backoffice-New-Api/EducationModule/AddQuestionWithOption.js
+++ src/api/routes/Backoffice-New-Api/EducationModule/AddQuestionWithOption.js
@@ -39,7 +39,28 @@ router.post("/", function(request, response){
                 });
             })
 
+    
+            req.body.options.forEach(option => {
+                req.input('questionid',sql.Int, questionid);
+                req.input('optiontext',sql.NVarChar(200), optiontext);
+                req.input('iscorrect',sql.NVarChar(200), iscorrect);
+                req.input('orderno',sql.Int, orderno);
+                req.input('createdon',sql.NVarChar(100), createdon);
+                req.input('updatedon',sql.NVarChar(100), updatedon);
+                req.input('status',sql.Int, status);
+
+                req.execute("dbo.Add_QuestionOptionMaster").then(function(data){
+                    response.status(200).json({
+                        data: data.recordset
+                    })
+                }).catch((err)=>{
+                    console.log("Error while executing the SP - [error] " + err);
+                    response.status(404).json({
+                        data:err.message
+                    });
+                })
             
+            });
     
 
     }catch (err){
