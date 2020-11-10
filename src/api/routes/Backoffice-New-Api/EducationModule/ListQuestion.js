599ad97eeaa9cad2904e006d734083b3ae88d888
Your Name <you@example.com>
2020-11-10 18:21:01 +0530

    question listing api

--- src/api/routes/Backoffice-New-Api/EducationModule/ListQuestion.js
+++ src/api/routes/Backoffice-New-Api/EducationModule/ListQuestion.js
@@ -6,7 +6,8 @@ const dbConnection = require("../../../../utilities/db1");
 
 
 router.get("/", function(request, response){
-   
+
+    var QuestionList,OptionList;
     var chapterid = request.query.chapterid;
 
     console.log(request.query.chapterid);
@@ -16,18 +17,39 @@ router.get("/", function(request, response){
          
         req.input('chapterid',sql.Int, chapterid);
 
-        req.execute("dbo.Get_QuestionMaster", function(err, data){
-            if(err){
+        req.execute("dbo.Get_QuestionMaster").then((data)=>{
+
+                QuestionList = data.recordset;
+                console.log(QuestionList);
+                const req2 = new sql.Request(dbConnection)
+
+                req2.execute("dbo.Get_QuestionOptionMasterAll").then((data)=>{
+                    OptionList = data.recordset;
+                    var result =  QuestionList.map((que)=>{
+                        
+                        var options = OptionList.filter((opt)=>{
+                            if(opt.fld_questionid == que.fld_id){
+                                return opt;
+                            }
+                        });    
+                        que['options'] = options;
+                        return que;
+                    });
+
+                    console.log(result);
+                    response.status(200).json({
+                        data: result
+                    });
+
+                }) 
+
+            }).catch((err)=>{
                 console.log("Error while executing the SP - [error] " + err);
                 response.status(404).json({
                     data:err.message
                 });
-            }else{
-                response.status(200).json({
-                    data: data.recordset
-                });
-            }
-        });
+            });
+    
 
     }catch (err){
         response.status(500);
